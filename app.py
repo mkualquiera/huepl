2a650f51a8732b4646d344d9f23d41c038c4bd85
Juan Pablo Ossa Zapata <ozjuanpa@gmail.com>
2021-09-17 12:52:44 -0500

    Fixed path

--- app.py
+++ app.py
@@ -60,7 +60,10 @@ async def run_code(websocket, filename, working_dir):
         f'chmod +x {os.path.join(working_dir, filename)}')
     info("Changed permissions!")
     try:
-        proc = await asyncio.create_subprocess_shell(f" script --flush --quiet --return code.txt --command '{os.path.join('code', filename)}'",
+        #proc = await asyncio.create_subprocess_shell(f" script --flush --quiet --return code.txt --command '{os.path.join('code', filename)}'",
+        #                                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        proc = await asyncio.create_subprocess_shell(f"cd {working_dir}; ./{filename}'",
                                                      stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
         stdrelay = asyncio.create_task(relay_stream(proc,
